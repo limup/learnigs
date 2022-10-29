461d996b90255077d61e6fff296b1696086f1cdb
Arnaldo Vieira <limup@outlook.com>
2022-10-28 23:27:37 -0300

    ci:a dd publish storybook workflow

--- lab-ds/.storybook/main.cjs
+++ lab-ds/.storybook/main.cjs
@@ -14,5 +14,10 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/learnigs/'
+    }
   }
 }
