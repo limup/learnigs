626458a07a5782602dc2576c56e8cd1e4693dade
Arnaldo Vieira <limup@outlook.com>
2022-11-18 15:12:39 -0300

    Modifing home page react

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -2,12 +2,8 @@ import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-gray-500'>Enzo Soares de Lima</h1>
+    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100'>
 
-      <button className='bg-cyan-300 font-medium px-4 py-2  rounded text-white hover:bg-cyan-500'>
-        Sou Eu!
-      </button>
-    </>
+    </div>
   )
 }
