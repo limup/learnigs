a33fc50b736745467068f9e3c686d9ecfccb9fc2
Arnaldo Vieira <limup@outlook.com>
2023-06-13 21:54:56 -0300

    Criado os scripts da EF para popular os produtos e categorias

---
+++ dotNet/APICatalogo/Migrations/20230614003701_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagenUrl) Values('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagenUrl) Values('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagenUrl) Values('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ dotNet/APICatalogo/Migrations/20230614004258_PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,Data,CategoriaId) " +
+            "Values('Coca-cola Diet','Refrigerante de Cola 350 ml',5.45,'cocacola.jpg',50,now(),1)");
+            
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,Data,CategoriaId) " +
+            "Values('Lanche de Atum','Lanche de Atum com maionese',8.45,'atum.jpg',10,now(),2)");
+
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,Data,CategoriaId) " +
+            "Values('Pudim 100g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Produtos");
+        }
+    }
+}
