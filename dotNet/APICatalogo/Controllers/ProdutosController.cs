6faed35338c070a05b6e02d867099aa5642a35d8
Arnaldo Vieira <limup@outlook.com>
2023-06-13 22:43:32 -0300

    Incluído os verbos get no controller Produtos

---
+++ dotNet/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            return produto;
+        }
+    }
+}
