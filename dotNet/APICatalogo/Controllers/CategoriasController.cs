1c1fb5aacfd54c70531acd8d4ebcb315a1465858
Arnaldo Vieira <limup@outlook.com>
2023-06-21 20:00:24 -0300

    Incluído o verbo GetCategoriasProdutos

--- dotNet/APICatalogo/Controllers/CategoriasController.cs
+++ dotNet/APICatalogo/Controllers/CategoriasController.cs
@@ -16,6 +16,12 @@ namespace APICatalogo.Controllers
             _context = context;
         }
 
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(p => p.Produtos).ToList();
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<Categoria>> Get()
         {
