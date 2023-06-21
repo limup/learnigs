3e78a545dda3d2d4b0333751974347e19bbae748
Arnaldo Vieira <limup@outlook.com>
2023-06-21 19:34:11 -0300

    Incluído o verbo delete e o gitignore do dotnet

--- dotNet/APICatalogo/Controllers/ProdutosController.cs
+++ dotNet/APICatalogo/Controllers/ProdutosController.cs
@@ -66,5 +66,19 @@ namespace APICatalogo.Controllers
 
             return Ok(produto);
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+
+            if (produto is null)
+                return NotFound("Produto não localizado...");
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
