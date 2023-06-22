c1cbe5a1cfa727eaf81516f3b62eb7a0191af463
Arnaldo Vieira <limup@outlook.com>
2023-06-21 21:56:50 -0300

    Incluído o verbo http get e post e o pacote in Memory do EF Core

--- dotNet/APITarefas/Program.cs
+++ dotNet/APITarefas/Program.cs
@@ -1,9 +1,15 @@
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TarefaDB"));
 
 var app = builder.Build();
 
@@ -16,6 +22,14 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Olá Mundo");
 app.MapGet("frases", async () => await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
+
+app.MapGet("/tarefas", async (AppDbContext db) =>  await db.Tarefas.ToListAsync());
+
+app.MapPost("tarefas", async (Tarefa tarefa, AppDbContext db) => {
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
 
 // app.UseHttpsRedirection();
 
@@ -45,3 +59,18 @@ app.Run();
 // {
 //     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 // }
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
