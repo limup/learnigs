5bf9c226b0c725e68794ba50553a270ea124b746
Arnaldo Vieira <limup@outlook.com>
2023-06-26 16:24:47 -0300

    Incluído os verbos get com id, put e delete

--- dotNet/APITarefasDapper/Endpoints/TarefasEndpoints.cs
+++ dotNet/APITarefasDapper/Endpoints/TarefasEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Versioning;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,43 @@ public static class TarefasEndpoints
                 return Results.NotFound();
 
             return Results.Ok(tarefas);
+        });
+
+        app.MapGet("/tarefas/{id}", async (GetConnetcion connectionGetter, int id) => 
+        {
+            // using var con = await connectionGetter();
+            // var tarefa = con.Get<Tarefa>(id);
+            // return Results.Ok(tarefa);
+            
+            using var con = await connectionGetter();
+            return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa):Results.NotFound();
+
+        });
+
+        app.MapPost("/tarefas", async (GetConnetcion connectionGetter, Tarefa tarefa) =>
+        {
+            using var con = await connectionGetter();
+            var id = con.Insert(tarefa);
+            return Results.Created($"/tarefas/{id}", tarefa);
+        });
+
+        app.MapPut("/tarefas", async (GetConnetcion connectionGetter, Tarefa tarefa) =>
+        {
+            using var con = await connectionGetter();
+            var id = con.Update(tarefa);
+            return Results.Ok();
+        });
+
+        app.MapDelete("/tarefas/{id}", async (GetConnetcion connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            var deleted = con.Get<Tarefa>(id);
+
+            if(deleted is null)
+                return Results.NotFound();
+            
+            con.Delete(deleted);
+            return Results.Ok(deleted);
         });
     }
 }
