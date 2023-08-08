975d8cd0f6abf9d3ed0dbe0d94549665b9e75718
Arnaldo Vieira <limup@outlook.com>
2023-08-08 10:33:21 -0300

    add new project using RestSharp

--- dotNet/ConsoleAuthCerPrivateRestSharp/Program.cs
+++ dotNet/ConsoleAuthCerPrivateRestSharp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+using RestSharp;
+
+// Caminho do arquivo PFX contendo o certificado e a chave privada
+string pfxFilePath = "certificado.pfx";
+string pfxPassword = "geradua";
+
+string certPath = Path.Combine(Environment.CurrentDirectory, pfxFilePath);
+
+// Carrega o certificado e a chave privada do arquivo PFX
+X509Certificate2 clientCertificate = new(certPath, pfxPassword);
+
+var options = new RestClientOptions("https://api-pix-sandbox.banestes.b.br")
+{
+    MaxTimeout = -1,
+    ClientCertificates = new X509CertificateCollection { clientCertificate },
+};
+
+var client = new RestClient(options);
+var request = new RestRequest("/oauth/v1/access-token", Method.Post);
+request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+request.AddHeader("Authorization", "Basic ZDAzYWMyYzUtYTU5ZC00OWIxLWIxYmEtMzkwZmQyYTI4MTQ3OmY0YWFlZWM1LTUwNGEtNGVlMS1hYzkwLThlMDFhMTE3MzA4Yw==");
+request.AddParameter("grant_type", "client_credentials");
+
+RestResponse response = await client.ExecuteAsync(request);
+Console.WriteLine(response.Content);
+
