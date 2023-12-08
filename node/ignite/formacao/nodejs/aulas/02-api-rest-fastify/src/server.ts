8119fed56672dd31f7b37066e8a357d433c580a9
Arnaldo Vieira <limup@outlook.com>
2023-12-08 17:21:37 -0300

    Incluido o gitignore

---
+++ node/ignite/formacao/nodejs/aulas/02-api-rest-fastify/src/server.ts
@@ -0,0 +1,18 @@
+import { fastify } from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Runnig!')
+  })
