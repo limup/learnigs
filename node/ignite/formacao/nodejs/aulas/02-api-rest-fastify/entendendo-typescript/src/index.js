8974ec6e87a00d52a04c4996f45aa64e469c06b1
Arnaldo Vieira <limup@outlook.com>
2023-12-07 21:14:25 -0300

    Implementando o fastify

---
+++ node/ignite/formacao/nodejs/aulas/02-api-rest-fastify/entendendo-typescript/src/index.js
@@ -0,0 +1,11 @@
+function calculateAgeOfYears(user) {
+    return new Date().getFullYear() - user.birthYear;
+}
+calculateAgeOfYears({
+    birthYear: 1992
+});
+
+//Como o node não entende de typescripts, nós devemos instalar a dependência dele para que o node converta os .ts em .js
+//Instalando a dependencia com o comando: npm install -D typescript
+//Após isso, devemos inicializar o tsc: npx tsc --init
+//Comando para converter o arquivo .ts em .js: npx tsc src/index.ts
